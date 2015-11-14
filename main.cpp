d926669da886404c4f4444b39d44008d39158c6e
Blackrush <blackrushx@gmail.com>
2015-11-14 14:41:07 +0100

    Clean-up main

--- main.cpp
+++ main.cpp
@@ -8,21 +8,11 @@ using namespace std;
 
 int main() {
     ifstream f("Source.slang");
+
     Lexer lexer(f);
-//    while (true) {
-//        const Lex* lex = lexer.next();
-//        if (lex->lexName == "LEX") {
-//            cout << "\"" << static_cast<const StrLex*>(lex)->str << "\"";
-//            delete lex;
-//        } else if (lex->lexName == "EOF") {
-//            break;
-//        } else {
-//            cout << lex->lexName;
-//        }
-//        cout << endl;
-//    }
     Parser parser(lexer);
     Interpreter interpreter;
+
     while (true) {
         const Expression* expression = parser.next();
         if (expression->getType() == Expressions::NIL) {
